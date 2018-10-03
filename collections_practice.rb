dc64b28553d0acbdf7dcd8ec950715ff28db6373
Omid Vatandoost <omidvatan94@gmail.com>
2018-10-03 17:17:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,55 +1,53 @@
 def begins_with_r(array)
-  output1 = false
-  output2 = true
-  array.each {|x|
-  if x.start_with?("r")
-    output1 = true
-  else
-    output2 = false
+  array.all? do |word|
+    word.downcase.chr == "r"
   end
-  }
-return (output1 and output2)
 end
 
-
-def contain_a(array)
-  output = []
-  array.each {|x|
-  if x.include?("a")
-    output << x
-  else
-
-  end }
-  output
+def contain_a (array)
+  array.select {|element| element.include?("a")}
 end
 
 def first_wa(array)
-  input = []
-  output = ""
-  array.each { |x| input << x.to_s }
-  input.each { |y|
-  if y.start_with?("wa")
-    output << y
-    break
-  else
-  end
-  }
-return output
+  array.find {|word| word.to_s.start_with?("wa")}
 end
 
 def remove_non_strings(array)
-  output = []
-  array.each { |x|
-  if x.is_a? String
-    output << x
-  else
-  end
-  }
-return output
+  array.reject {|element| element.class != String}
 end
 
 
-  
+def count_elements(array)
+  counts = Hash.new(0)
+  array.collect {|element| counts[element]+=1 }
+    counts.collect do |hash, number|
+      hash[:count] = number
+    end
+  counts.keys
+end
 
-  
-  
+def merge_data(keys, data)
+  keys.each do |name_hash|
+    data.each do |hash|
+      name_hash.merge!(hash[name_hash[:first_name]])
+    end
+  end
+end
+
+def find_cool(array)
+  array.select {|entry| entry if entry.has_value?("cool")}
+end
+
+def organize_schools(schools)
+  by_location = {}
+    schools.each do |school, location_hash|
+      location_hash.each do |symbol, location|
+        if by_location[location] == nil
+          by_location[location] = [school]
+        else
+          by_location[location] << school
+        end
+      end
+    end
+    by_location
+end
